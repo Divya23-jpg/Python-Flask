18a76f3fdb2d898ef2c3433bde6132179fa1efe6
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-29 01:39:33 +0530

    divya

--- login.py
+++ login.py
@@ -7,12 +7,12 @@ app.secret_key="supersecret"
 @app.route("/",methods=["GET","POST"])
 def login():
     if request.method=="POST":
-        username=request.form.get("Username")
-        password=request.form.get("Password")
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        if username=="admin" and password=="123":
+        if username=="admin" and password =="123":
             session["user"]=username #store in session
-            return redirect(url_for("Welcome"))
+            return redirect(url_for("welcome"))
 
         else:
             # mimetype="tesxt/plain gives html 
@@ -20,7 +20,7 @@ def login():
         
     return'''
         <h2>Login Page</h2>
-        <form method="POST>
+        <form method="POST">
         Username : <input type="text" name="username"><br>
         Password : <input type="text" name="password"><br>
         <input type="submit" value="Login">
@@ -34,7 +34,8 @@ def welcome():
     if "user" in session:
         return f'''
             <h2> Welcome,{session["user"]}!</h2>
-            <a hred={url_for('logout')}>Logout</a>
+           
+            <a href="{url_for('logout')}">Logout</a>
     '''
     return redirect(url_for("login"))
 
