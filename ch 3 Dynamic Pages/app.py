5803592df2f6ac64867d09057f293dac7d4e51b4
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-30 01:02:15 +0530

    divya

--- ch 3 Dynamic Pages/app.py
+++ ch 3 Dynamic Pages/app.py
@@ -1,27 +1,12 @@
-from flask import Flask,render_template , request
+from flask import Flask , render_template
 
 app=Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template("login.html")
-
-@app.route("/submit",methods=["POST"])
-def submit():
-    username=request.form.get("username")
-    password=request.form.get("password")
-    
-    #? For Only 1 User 
-    """if username=="Divya" and password=="123":
-        return render_template("welcome.html",name=username)"""
-   
-    # ? For Many valid User 
-    valid_users={
-        "admin":"123",
-        "Divya": "123",
-        "Rohan": "0795"
-    }
-    if username in valid_users and password==valid_users[username]:
-        return render_template("welcome.html",name=username)
-    else:
-        return "Invalid credentials"
+def student_profile():
+    return render_template(
+        "profile.html",
+        name="Arun",
+        is_topper=True,
+        subjecst=["Maths","Science","History"]
+        )
