a2f23ff138f9567b2267c852687d45b31151dfe9
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-04-30 01:50:59 +0530

    divya

--- ch 3 Dynamic Pages/app.py
+++ ch 3 Dynamic Pages/app.py
@@ -2,11 +2,22 @@ from flask import Flask , render_template
 
 app=Flask(__name__)
 
+# ? Student_profile
+# @app.route("/")
+# def student_profile():
+#     return render_template(
+#         "profile.html",
+#         name="Arun",
+#         is_topper=True,
+#         subjects=["Maths","Science","History"]
+#         )
+
+# ? Home Page with template extends
+
 @app.route("/")
-def student_profile():
-    return render_template(
-        "profile.html",
-        name="Arun",
-        is_topper=True,
-        subjects=["Maths","Science","History"]
-        )
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
