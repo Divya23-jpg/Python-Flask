70a014301aeb6324d6bab4cfac76dfc53fe5da76
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 12:05:13 +0530

    divya

--- Project (To Do List using Flask)/Todo_app/app/models.py
+++ Project (To Do List using Flask)/Todo_app/app/models.py
@@ -8,3 +8,4 @@ class Task(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(100),nullable=False)
     status=db.Column(db.String(20), default="Pending")
+    
