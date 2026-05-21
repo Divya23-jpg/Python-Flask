8a04e3dc5b5b4d5ec3f2b05a5f841512e6cc6a34
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-21 12:27:11 +0530

    divya

---
+++ Project (To Do List using Flask)/Todo_app/app/routes/tasks.py
@@ -0,0 +1,54 @@
+from flask  import Blueprint,redirect,render_template,session,flash,url_for,request
+from app import db
+from app.models import Task
+
+
+tasks_bp=Blueprint('tasks',__name__)
+
+@tasks_bp.route('/')
+def view_tasks():
+    if 'user' not in session:
+        return redirect(url_for('auth.login'))
+    
+    tasks=Task.query.all()
+    return render_template('tasks.html',tasks=tasks)
+
+
+@tasks_bp.route('/add,',methods=["POST"])
+def add_tasks():
+    if 'user' not in session:
+        return redirect(url_for('auth.login'))
+
+    title=request.form.get('title')
+    if title:
+        new_tasks=Task(title=title,status='Pending')
+        db.add(new_tasks)
+        db.session.commit()
+        flash("Task Added Successfully",'success')
+
+    return redirect(url_for('tasks.view_tasks'))
+
+
+@tasks_bp.route('/toggle/<int:task_id>',methods=["POST"])
+def toggle_status(task_id):
+    task=Task.query.get(task_id)
+    if task:
+        if task.status=="Pending":
+            task.status=="Working"
+
+        elif task.status=="Working":
+            task.status=="Done"
+
+        else:
+            task.staus=="Pending"
+        db.session.commit()
+
+    return redirect(url_for('tasks.view_tasks'))
+
+
+@tasks_bp.route('/clear',methods=["POST"])
+def clear_tasks():
+    Task.query.delete()
+    db.session.commit()
+    flash("All Tasks Cleared!",'info')
+    return redirect(url_for('tasks.view_tasks'))
